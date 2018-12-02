99cfe36f86449c4f5b445eb92ade4abc92ed5de8
SILexRaze <fonne.victor@gmail.com>
2018-12-02 16:22:57 +0100

    ajout d'un dossier log

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Fillit_unit_test
-n'oubliez pas de mettre le path de votre executable fillit dans le script
+
+## Usage :
+
+Clonez le repo dans le dossier qui contient votre executable se nommant fillit.
+Lancer la commande :
+```
+sh fillit_unit_test/sh_test.sh
+```
+
+## Format :
+
+La sortie de ce script correspond a la sortie de votre programme avec quelques lignes en plus contenant des indications sur les leaks (un petit grep dans la sortie de valgrind).
+
+## Logs :
+
+Vous retrouverez un log par test dans le dossier fillit_unit_test
--- sh_test.sh
+++ sh_test.sh
@@ -1,17 +1,16 @@
 #!/bin/zsh
-#INDIQUER ICI LE PATH DE VOTRE EXECUTABLE
-$ex = PATH DE VOTRE EXE
+mkdir fillit_test_log
 for i in `seq 0 21`;
 do
 echo "\n\n=== $i | "valid_$i" ====\n"
-valgrind --leak-check=full --log-file="log" ./$ex "correct_file/valid_$i"
+valgrind --leak-check=full --log-file="fillit_test_log/log$1.txt" ../fillit "correct_file/valid_$i"
 grep "definitely lost:" log
 sleep 1
 done
-
 for i in `seq 0 25`;
 do
 echo "\n\n=== $i | "error_$i" ====\n"
-valgrind --leak-check=full --log-file="log" ./$ex "error_file/error_$i"
+valgrind --leak-check=full --log-file="fillit_test_log/log$1.txt" ../fillit "error_file/error_$i"
 grep "definitely lost:" log
 done
+echo "created by vifonne"
