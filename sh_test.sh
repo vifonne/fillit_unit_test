b6c3f617ff9b8cbe37d7b362ec953c42532f3d98
SILexRaze <fonne.victor@gmail.com>
2018-12-04 15:32:50 +0100

    add a 27 testri test

--- sh_test.sh
+++ sh_test.sh
@@ -7,7 +7,7 @@ valgrind --leak-check=full --log-file="fillit_test_log/log_correctfile$i.txt" ./
 grep "definitely lost:" fillit_test_log/log_correctfile$i.txt
 sleep 1
 done
-for i in `seq 0 25`;
+for i in `seq 0 26`;
 do
 echo "\n\n=== $i | "error_$i" ====\n"
 valgrind --leak-check=full --log-file="fillit_test_log/log_errorfile$i.txt" ./fillit "fillit_unit_test/error_file/error_$i"
